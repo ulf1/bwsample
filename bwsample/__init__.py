6f2b5acc4c38bee4c7cb522bf3d65109d060b515
UH <554c46@gmail.com>
2021-02-13 14:45:34 +0100

    process batches

--- bwsample/__init__.py
+++ bwsample/__init__.py
@@ -1,5 +1,6 @@
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .sampling import (indices_overlap, indices_twice)
-from .counting import (extract_pairs)
+from .counting import (
+    extract_pairs, extract_pairs_batch, extract_pairs_batch2)
 from .utils import (to_scipy)
