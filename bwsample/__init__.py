c6dc53d6902b6f5454a47f2cfc5d4a58411a7cc1
UH <554c46@gmail.com>
2021-02-15 03:04:56 +0100

    ranking.py replaces scaling.py, transition matrix algo added, p-value based metric changed

--- bwsample/__init__.py
+++ bwsample/__init__.py
@@ -1,8 +1,8 @@
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 from .sampling import (indices_overlap, indices_twice)
 from .counting import (
     extract_pairs, extract_pairs_batch, extract_pairs_batch2,
     logical_infer)
 from .utils import (to_scipy)
-from .scaling import (scale_simple, scale_pvalues)
+from .ranking import (ranking)
