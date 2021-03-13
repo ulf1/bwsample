532f66d5392e54eb3915eb2f3b999aa02284f757
UH <554c46@gmail.com>
2021-03-13 18:11:10 +0100

    ranking.py refactored

--- test/test_rank.py
+++ test/test_rank.py
@@ -3,46 +3,44 @@ import bwsample as bws
 
 def test1():
     # demo data
-    data = (
+    evaluations = (
         ([1, 0, 0, 2], ['A', 'B', 'C', 'D']),
         ([1, 0, 0, 2], ['A', 'B', 'C', 'D']),
         ([2, 0, 0, 1], ['A', 'B', 'C', 'D']),
         ([0, 1, 2, 0], ['A', 'B', 'C', 'D']),
         ([0, 1, 0, 2], ['A', 'B', 'C', 'D']),
     )
-    dok, _ = bws.counting.direct_extract_batch(data)
+    dok, _, _, _, _ = bws.count(evaluations)
 
     # possible settings
     settings = [
-        {"method": "ratio", "avg": "all", "calibration": "platt"},
-        {"method": "ratio", "avg": "exist", "calibration": "platt"},
-        {"method": "ratio", "avg": "all", "calibration": "isotonic"},
-        {"method": "ratio", "avg": "exist", "calibration": "isotonic"},
-        {"method": "ratio", "avg": "all", "calibration": "minmax"},
-        {"method": "ratio", "avg": "exist", "calibration": "minmax"},
-        {"method": "pvalue", "avg": "all", "calibration": "platt"},
-        {"method": "pvalue", "avg": "exist", "calibration": "platt"},
-        {"method": "pvalue", "avg": "all", "calibration": "isotonic"},
-        {"method": "pvalue", "avg": "exist", "calibration": "isotonic"},
-        {"method": "pvalue", "avg": "all", "calibration": "minmax"},
-        {"method": "pvalue", "avg": "exist", "calibration": "minmax"},
-        {"method": "btl", "calibration": "platt"},
-        {"method": "btl", "calibration": "isotonic"},
-        {"method": "btl", "calibration": "minmax"},
-        {"method": "orme", "calibration": "platt"},
-        {"method": "orme", "calibration": "isotonic"},
-        {"method": "orme", "calibration": "minmax"},
-        {"method": "eigen", "calibration": "platt"},
-        {"method": "eigen", "calibration": "isotonic"},
-        {"method": "eigen", "calibration": "minmax"},
-        {"method": "transition", "calibration": "platt"},
-        {"method": "transition", "calibration": "isotonic"},
-        {"method": "transition", "calibration": "minmax"}
+        {"method": "ratio", "avg": "all", "adjust": "platt"},
+        {"method": "ratio", "avg": "exist", "adjust": "platt"},
+        {"method": "ratio", "avg": "all", "adjust": "quantile"},
+        {"method": "ratio", "avg": "exist", "adjust": "quantile"},
+        {"method": "ratio", "avg": "all", "adjust": "sig3iqr"},
+        {"method": "ratio", "avg": "exist", "adjust": "sig3iqr"},
+        {"method": "pvalue", "avg": "all", "adjust": "platt"},
+        {"method": "pvalue", "avg": "exist", "adjust": "platt"},
+        {"method": "pvalue", "avg": "all", "adjust": "quantile"},
+        {"method": "pvalue", "avg": "exist", "adjust": "quantile"},
+        {"method": "pvalue", "avg": "all", "adjust": "sig3iqr"},
+        {"method": "pvalue", "avg": "exist", "adjust": "sig3iqr"},
+        {"method": "btl", "adjust": "platt"},
+        {"method": "btl", "adjust": "quantile"},
+        {"method": "btl", "adjust": "sig3iqr"},
+        {"method": "eigen", "adjust": "platt"},
+        {"method": "eigen", "adjust": "quantile"},
+        {"method": "eigen", "adjust": "sig3iqr"},
+        {"method": "trans", "adjust": "platt"},
+        {"method": "trans", "adjust": "quantile"},
+        {"method": "trans", "adjust": "sig3iqr"}
     ]
 
     # loop over each setting
     for setting in settings:
-        ranked, ordids, scores, _ = bws.rank(dok, **setting)
-        assert len(ranked) == 4
-        assert len(ordids) == 4
+        positions, sortedids, metrics, scores, info = bws.rank(dok, **setting)
+        assert len(positions) == 4
+        assert len(sortedids) == 4
+        assert len(metrics) == 4
         assert len(scores) == 4
