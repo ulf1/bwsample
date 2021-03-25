d9f0849c557693b04ddf8eb8bdcac5ddc0a349f9
UH <554c46@gmail.com>
2021-03-25 09:52:03 +0100

    fix #54

--- bwsample/__init__.py
+++ bwsample/__init__.py
@@ -1,6 +1,7 @@
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 from .sampling import sample
 from .counting import count
 from .ranking import rank
 from .utils import (to_scipy, add_dok, adjustscore)
+from .maxdiff import scoring_orme
